d99d4a023e065992e8a4dce94955e97dadcf1619
Ridwan Tajudeen <108151117+Ridwantajudeen@users.noreply.github.com>
2022-09-11 20:11:14 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,24 @@
-edit
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints all the numbers of base 16 in lowercase
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	char c;
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
