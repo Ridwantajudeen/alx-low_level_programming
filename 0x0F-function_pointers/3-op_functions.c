0fd416c202570bffc44378318de2f71064b44a7b
Ridwan Tajudeen <108151117+Ridwantajudeen@users.noreply.github.com>
2022-10-15 12:20:31 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,39 +1,67 @@
-#include <stdio.h>
 #include "3-calc.h"
 
 /**
-* get_op_func - a  function that selects the correct function to perform the
-* operation asked by the user. You’re not allowed to declare any other
-* function
-* @s: operator passed as argument to the program
-*
-* Return: a pointer to the function that corresponds to the operator given as a
-* parameter
-*/
+ * op_add - addition operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
 
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-	};
+	return (a + b);
+}
 
-	int i;
+/**
+ * op_sub - subtraction operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
 
-	i = 0;
-	while (ops[i].op)
-	{
-		if (*(ops[i].op) == *s)
-		{
-			return (ops[i].f);
-		}
-	     i++;
-	}
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
-	return (0);
+/**
+ * op_mul - multiplication operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
 
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * op_mod - modulo operation
+ * @a: first term
+ * @b: second term
+ *
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
